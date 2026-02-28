7a17a9af2960da4201bee052260d138a933ed831
Brayan Quebrada <alexisquebrada11@gmail.com>
2026-02-27 22:12:17 -0500

    feature(Lugares) crud para lugares y solo los ADMIN pueden usarlos

--- src/main/java/com/cesde/eventhub/servicio/LugarServicio.java
+++ src/main/java/com/cesde/eventhub/servicio/LugarServicio.java
@@ -1,10 +1,13 @@
 package com.cesde.eventhub.servicio;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
+import com.cesde.eventhub.dto.ActualizarLugarDTO;
 import com.cesde.eventhub.dto.LugarDTO;
 import com.cesde.eventhub.mapper.LugarMapper;
 import com.cesde.eventhub.modelos.Lugar;
@@ -32,8 +35,45 @@ public class LugarServicio {
 	
 	@Secured("ROLE_ADMIN")
 	public Page<Lugar> lugaresActivos(Pageable pageable){
-		
-		
+	
 		return lugarRepositorio.findByActivoTrue(pageable);
 	}
+	
+	@Secured("ROLE_ADMIN")
+	public ActualizarLugarDTO actualizarLugar(Long id, ActualizarLugarDTO lugarAct) {
+		
+		Optional<Lugar> lugarOpcional = lugarRepositorio.findById(id);
+		
+		if(!lugarOpcional.isPresent()) {
+			throw new RuntimeException("No existe un lugar con el id: " + id);
+		}
+		
+		Lugar lugarEncontrado = lugarOpcional.get();
+		
+		lugarEncontrado.setNombre(lugarAct.getNombre());
+		lugarEncontrado.setCiudad(lugarAct.getCiudad());
+		lugarEncontrado.setCapacidad_total(lugarAct.getCapacidad_total());
+		lugarEncontrado.setDescripcion(lugarAct.getDescripcion());
+		lugarEncontrado.setDireccion(lugarAct.getDireccion());
+		lugarEncontrado.setActivo(lugarAct.getActivo());
+		lugarEncontrado.setImagenUrl(lugarAct.getImagenUrl());
+		
+		lugarRepositorio.save(lugarEncontrado);
+		
+		return mapper.haciaDTOAct(lugarEncontrado);
+	}
+	
+	@Secured("ROLE_ADMIN")
+	public void eliminarLugar(Long id) {
+		
+		Optional<Lugar> lugarABorrar = lugarRepositorio.findById(id);
+		if(lugarABorrar.isEmpty()) {
+			throw new RuntimeException("No se pudo encontrar el lugar con id: "+ id);
+		}
+		
+		Lugar lugarBorrado = lugarABorrar.get();	
+		lugarRepositorio.delete(lugarBorrado);
+	}
+	
+	
 }
