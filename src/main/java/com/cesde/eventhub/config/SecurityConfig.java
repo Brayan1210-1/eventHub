925c7a3f1bfcc8abe6a63db24de7123e05da3a2c
Brayan Quebrada <alexisquebrada11@gmail.com>
2026-04-27 15:12:31 -0500

    cors funcional y refactor en docker-compsoe

--- src/main/java/com/cesde/eventhub/config/SecurityConfig.java
+++ src/main/java/com/cesde/eventhub/config/SecurityConfig.java
@@ -72,9 +72,9 @@ public class SecurityConfig {
 	    CorsConfigurationSource corsConfigurationSource() {
 	        CorsConfiguration configuration = new CorsConfiguration();
 	       
-	        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); 
+	        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8081"));
 	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-	        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+	        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 	        configuration.setAllowCredentials(true); 
 	        
 	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
